34016100a725d38c1f7b926a77358f1fb40c10b8
Anastasiia Sycheva <ansycheva26@gmail.com>
2025-10-04 17:19:03 +0200

    Add dna-rna module

--- modules/dna_rna_tools.py
+++ modules/dna_rna_tools.py
@@ -1,7 +1,14 @@
-def get_nucleic_acid_type(sequence,
-                          alphabet_dna={'A', 'T', 'G', 'C'},
-                          alphabet_rna={'A', 'U', 'G', 'C'}):
+alphabet_dna={'A', 'T', 'G', 'C'}
+alphabet_rna={'A', 'U', 'G', 'C'}
+dna_compl={'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G',
+           'a': 't', 't': 'a', 'g': 'c', 'c': 'g'}
+rna_compl={'A': 'U', 'U': 'A', 'G': 'C', 'C': 'G',
+           'a': 'u', 'u': 'a', 'g': 'c', 'c': 'g'}
+mrna_compl={'A': 'A', 'T': 'U', 'C': 'C', 'G': 'G',
+            'a': 'a', 't': 'u', 'c': 'c', 'g': 'g'}
 
+
+def get_nucleic_acid_type(sequence, alphabet_dna=alphabet_dna, alphabet_rna=alphabet_rna):
     unique_chars = set(sequence.upper())
 
     if unique_chars <= alphabet_dna:
@@ -13,15 +20,11 @@ def get_nucleic_acid_type(sequence,
 
 
 def is_nucleic_acid(sequence):
-
     seq_type = get_nucleic_acid_type(sequence)
     return seq_type in ['DNA', 'RNA']
 
 
-def transcribe(sequence,
-               mrna_compl={'A': 'A', 'T': 'U', 'C': 'C', 'G': 'G',
-                           'a': 'a', 't': 'u', 'c': 'c', 'g': 'g'}):
-
+def transcribe(sequence, mrna_compl=mrna_compl):
     seq_type = get_nucleic_acid_type(sequence)
 
     if seq_type == 'DNA':
@@ -31,31 +34,24 @@ def transcribe(sequence,
             transcribe_seq += rna_nt
         return transcribe_seq
 
-    elif seq_type == 'RNA':
+    if seq_type == 'RNA':
         transcribe_seq = sequence
         return transcribe_seq
-    else:
-        print("Warning:", sequence, "is not a nucleotide sequence! Return None")
-        return None
+
+    print("Warning:", sequence, "is not a nucleotide sequence! Return None")
+    return None
 
 
 def reverse(sequence):
-
     if is_nucleic_acid(sequence):
         reverse_seq = sequence[::-1]
         return reverse_seq
 
-    else:
-        print("Warning:", sequence, "is not a nucleotide sequence! Return None")
-        return None
+    print("Warning:", sequence, "is not a nucleotide sequence! Return None")
+    return None
 
 
-def get_complement(sequence,
-                   dna_compl={'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G',
-                              'a': 't', 't': 'a', 'g': 'c', 'c': 'g'},
-                   rna_compl={'A': 'U', 'U': 'A', 'G': 'C', 'C': 'G',
-                              'a': 'u', 'u': 'a', 'g': 'c', 'c': 'g'}):
-
+def get_complement(sequence, dna_compl=dna_compl, rna_compl=rna_compl):
     seq_type = get_nucleic_acid_type(sequence)
 
     if not seq_type:
@@ -82,47 +78,10 @@ def get_complement(sequence,
 
 
 def get_reverse_complement(sequence):
-
     complement_seq = get_complement(sequence)
+
     if complement_seq is not None:
         reverse_complement_seq = reverse(complement_seq)
         return reverse_complement_seq
-    else:
-        return None
 
-
-def run_dna_rna_tools(*args,
-                      func_dict={'is_nucleic_acid': is_nucleic_acid,
-                                 'transcribe': transcribe,
-                                 'reverse': reverse,
-                                 'complement': get_complement,
-                                 'reverse_complement': get_reverse_complement}):
-
-    if not args:
-        print("Please provide arguments!")
-        return None
-
-    else:
-        procedure = args[-1]
-        seqs = args[0:-1]
-
-        if not seqs and procedure in func_dict:
-            print("Looks like you forgot to provide a nucleotide sequence(s). Please try again!")
-            return None
-
-        else:
-            if procedure in func_dict:
-                selected_function = func_dict[procedure]
-
-                if len(seqs) == 1:
-                    res = selected_function(seqs[0])
-                    return res
-                else:
-                    res = []
-                    for sq in seqs:
-                        res.append(selected_function(sq))
-                    return res
-
-            else:
-                print("Looks like you forgot to specify what to do with the sequence(s). Please try again!")
-                return None
+    return None
